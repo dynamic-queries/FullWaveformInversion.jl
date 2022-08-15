30a19e4f2ef3b74a05f0a97419648280717a269d
Rahul Manavalan <rahul.learns.physics@gmail.com>
2022-08-15 10:18:54 +0200

    Validaiton plots

--- src/validation.jl
+++ src/validation.jl
@@ -1,6 +1,9 @@
 using Flux
 using Flux:splitobs 
 using BSON: @load
+using HDF5
+using NeuralOperators
+using Plots
 
 # We know apriori that the problem was solved in the domain.
 # x ∈ [0,1]
@@ -16,7 +19,7 @@ Y = reshape([yi for _ in x for yi in y],(length(x),length(y)))
 ## Data
 filename = "./data/GROUND_TRUTH"
 
-batches = 1:80
+batches = 11:14
 BS = length(batches)
 nx = length(x)
 ny = length(y)
@@ -31,3 +34,15 @@ for (i,b) in enumerate(batches)
     xdata[3,:,:,i] .= Y
     ydata[1,:,:,i] .= read(file["100"])
 end     
+
+@load "src/os_checkpoints/checkpoint_epoch_139_loss_0.10179136244195063.bson" model 
+
+ypredict = model(xdata)
+
+# Validation
+for i=1:4
+    p1 = heatmap(x,y,ypredict[1,:,:,i],title="FNO model")
+    p2 = heatmap(x,y,ydata[1,:,:,i],title="Original")
+    display(plot(p1,p2,layout=(2,1),legend=false))
+    savefig("validation/$(i).png")
+end 
